cc218d672b0a6c524448e68fb0accf99a021f90f
Sahar Galimidi <sahargalimidi@gmail.com>
2024-07-29 21:55:59 +0300

    test build trigger

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.logger.info("Starting Flask app on port 8777")
     app.run(debug=True, host='0.0.0.0', port=8777)
