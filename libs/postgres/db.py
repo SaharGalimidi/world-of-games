3a29de776e0029be7f020d13f7cc420c84c94e8d
Sahar Galimidi <sahargalimidi@gmail.com>
2024-08-24 17:02:59 +0300

    added .env file for the workflow

--- libs/postgres/db.py
+++ libs/postgres/db.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
